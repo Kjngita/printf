b90ea92a04b26ee2655a7c3194c91b76ba000cbb
Giang <dollar_snowbell95@yahoo.com>
2025-05-09 00:10:43 +0300

    longer main

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
+	ft_printf("\tReturns: %d\tft_printf\n", ft_printf("%%"));
+	printf("\tReturns: %d\tprintf\n\n", printf("%%"));
+
+	ft_printf("\tReturns: %d\tft_printf\n", ft_printf("%c", 'G'));
+	printf("\tReturns: %d\tprintf\n\n", printf("%c", 'G'));
+
 	ft_printf("\tReturns: %d\tft_printf\n", ft_printf("%s", "texthas13char"));
-	printf("\tReturns: %d\tprintf\n", printf("%s", "texthas13char"));
+	printf("\tReturns: %d\tprintf\n\n", printf("%s", "texthas13char"));
 }
